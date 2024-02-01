1430f1463719a6d4be058580ad2c77e9c375d007
Erik Gooding <erik.gooding1@gmail.com>
2024-02-01 01:31:30 +0000

    Put each of the standings outputs into an embed object

--- commands/standings.js
+++ commands/standings.js
@@ -1,6 +1,7 @@
-const { SlashCommandBuilder, hyperlink } = require('discord.js');
+const { SlashCommandBuilder, hyperlink, EmbedBuilder } = require('discord.js');
 const axios = require('axios');
 const rootUrl = 'https://api-web.nhle.com/v1';
+
 
 async function getStandings(year, format) {
     // TODO: Add handling for when format value is provided
@@ -8,66 +9,152 @@ async function getStandings(year, format) {
 
     const processResponse = (response) => {
         response.data.standings.forEach(record => {
-            standings.push({ 
-                divisionName: record.divisionName, 
-                conferenceName: record.conferenceName, 
-                wildcardSequence: record.wildcardSequence, 
-                teamName: record.teamName.default, 
-                wins: record.wins, losses: record.losses, 
-                otLosses: record.otLosses, 
-                points: record.points 
+            standings.push({
+                divisionName: record.divisionName,
+                conferenceName: record.conferenceName,
+                wildcardSequence: record.wildcardSequence,
+                teamName: record.teamName.default,
+                teamAbbrev: record.teamAbbrev.default,
+                wins: record.wins, losses: record.losses,
+                otLosses: record.otLosses,
+                points: record.points
             });
         });
     }
 
     const formatStandings = (standings, format) => {
 
+        // Function to calculate padding for each property
+        function calculatePadding(value, maxWidth) {
+            const padding = maxWidth - value.toString().length + 1;
+            return ' '.repeat(padding);
+        }
+
+        // Calculate maximum widths for each property
+        const maxWidths = {
+            divisionName: Math.max(...standings.map(record => record.divisionName.length)),
+            conferenceName: Math.max(...standings.map(record => record.conferenceName.length)),
+            wildcardSequence: Math.max(...standings.map(record => record.wildcardSequence.toString().length)),
+            teamName: Math.max(...standings.map(record => record.teamName.length)),
+            teamAbbrev: Math.max(...standings.map(record => record.teamAbbrev.length), 6),
+            wins: Math.max(...standings.map(record => record.wins.toString().length), 3),
+            losses: Math.max(...standings.map(record => record.losses.toString().length), 3),
+            otLosses: Math.max(...standings.map(record => record.otLosses.toString().length), 4),
+            points: Math.max(...standings.map(record => record.points.toString().length), 3),
+        };
+
+        let standingsHeader = `\`TEAM${calculatePadding('TEAM', maxWidths.teamAbbrev)}W${calculatePadding('W', maxWidths.wins)}L${calculatePadding('L', maxWidths.losses)}OTL${calculatePadding('OTL', maxWidths.otLosses)}PTS${calculatePadding('PTS', maxWidths.points)}\n`;
+        //let buildRecordString(record) = `${record.teamAbbrev}${calculatePadding(record.teamAbbrev, maxWidths.teamAbbrev)}${record.wins}${calculatePadding(record.wins, maxWidths.wins)}${record.losses}${calculatePadding(record.losses, maxWidths.losses)}${record.otLosses}${calculatePadding(record.otLosses, maxWidths.otLosses)}${record.points}${calculatePadding(record.points, maxWidths.points)}\n`;
+        const buildRecordString = (record) => {
+            return `${record.teamAbbrev}${calculatePadding(record.teamAbbrev, maxWidths.teamAbbrev)}${record.wins}${calculatePadding(record.wins, maxWidths.wins)}${record.losses}${calculatePadding(record.losses, maxWidths.losses)}${record.otLosses}${calculatePadding(record.otLosses, maxWidths.otLosses)}${record.points}${calculatePadding(record.points, maxWidths.points)}\n`;
+        }
+
         const formatConferenceStandings = (standings) => {
-            console.log("Conference formatting");
-            let easternStandings = '';
-            let westernStandings = '';
+            let easternStandings = standingsHeader;
+            let westernStandings = standingsHeader;
             standings.forEach(record => {
-                if(record.conferenceName === 'Eastern') {
-                    easternStandings += `${record.teamName} - ${record.wins}W - ${record.losses}L - ${record.otLosses}OT - ${record.points}P\n`;
+                if (record.conferenceName === 'Eastern') {
+                    easternStandings += buildRecordString(record);
+
                 } else {
-                    westernStandings += `${record.teamName} - ${record.wins}W - ${record.losses}L - ${record.otLosses}OT - ${record.points}P\n`;
+                    westernStandings += buildRecordString(record);
                 }
             });
-            let formattedStandings = 'Eastern Conference:\n' + easternStandings + '\nWestern Conference:\n' + westernStandings;
+            easternStandings += '`\n';
+            westernStandings += '`\n';
+            let formattedStandings = 'Eastern Conference:\n' + easternStandings + 'Western Conference:\n' + westernStandings;
             return formattedStandings;
         }
 
         const formatWildcardStandings = (standings) => {
+            let atlanticStandings = standingsHeader;
+            let metropolitanStandings = standingsHeader;
+            let centralStandings = standingsHeader;
+            let pacificStandings = standingsHeader;
+            let eastWildcardStandings = standingsHeader;
+            let westWildcardStandings = standingsHeader;
+
+            standings.forEach(record => {
+                if (record.wildcardSequence == 0) {
+                    console.log(record.teamName);
+                    switch (record.divisionName) {
+                        case 'Atlantic':
+                            atlanticStandings += buildRecordString(record);
+                            break;
+                        case 'Metropolitan':
+                            metropolitanStandings += buildRecordString(record);
+                            break;
+                        case 'Central':
+                            centralStandings += buildRecordString(record);
+                            break;
+                        case 'Pacific':
+                            pacificStandings += buildRecordString(record);
+                            break;
+                        default:
+                            break;
+                    }
+                } else {
+                    switch (record.conferenceName) {
+                        case 'Eastern':
+                            eastWildcardStandings += buildRecordString(record);
+                            break;
+                        case 'Western':
+                            westWildcardStandings += buildRecordString(record);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                
+            });
+            atlanticStandings += '`\n';
+            metropolitanStandings += '`\n';
+            centralStandings += '`\n';
+            pacificStandings += '`\n';
+            eastWildcardStandings += '`\n';
+            westWildcardStandings += '`\n';
+            let formattedStandings = "East\n" + atlanticStandings + metropolitanStandings + eastWildcardStandings + "West\n" + centralStandings + pacificStandings + westWildcardStandings;
+            console.log(formattedStandings);
+            return formattedStandings;
         }
 
         const formatLeagueStandings = (standings) => {
-
+            let leagueStandings = standingsHeader;
+            standings.forEach(record => {
+                leagueStandings += buildRecordString(record);
+            });
+            leagueStandings += '`\n';
+            return leagueStandings;
         }
 
         const formatDivisionStandings = (standings) => {
-            let atlanticStandings = '';
-            let metropolitanStandings = '';
-            let centralStandings = '';
-            let pacificStandings = '';
+            let atlanticStandings = standingsHeader;
+            let metropolitanStandings = standingsHeader;
+            let centralStandings = standingsHeader;
+            let pacificStandings = standingsHeader;
             standings.forEach(record => {
                 switch (record.divisionName) {
                     case 'Atlantic':
-                        atlanticStandings += `${record.teamName} - ${record.points}\n`;
+                        atlanticStandings += buildRecordString(record);
                         break;
                     case 'Metropolitan':
-                        metropolitanStandings += `${record.teamName} - ${record.points}\n`;
+                        metropolitanStandings += buildRecordString(record);
                         break;
                     case 'Central':
-                        centralStandings += `${record.teamName} - ${record.points}\n`;
+                        centralStandings += buildRecordString(record);
                         break;
                     case 'Pacific':
-                        pacificStandings += `${record.teamName} - ${record.points}\n`;
+                        pacificStandings += buildRecordString(record);
                         break;
                     default:
                         break;
                 }
             });
-            let formattedStandings = 'Atlantic Division:\n' + atlanticStandings + '\nMetropolitan Division:\n' + metropolitanStandings + '\nCentral Division:\n' + centralStandings + '\nPacific Division:\n' + pacificStandings;
+            atlanticStandings += '`\n';
+            metropolitanStandings += '`\n';
+            centralStandings += '`\n';
+            pacificStandings += '`\n';
+            let formattedStandings = "Atlantic Division:\n" + atlanticStandings + "Metropolitan Division:\n" + metropolitanStandings + "Central Division:\n" + centralStandings + "Pacific Division:\n" + pacificStandings;
             return formattedStandings;
         }
 
@@ -91,12 +178,12 @@ async function getStandings(year, format) {
     let requestUrl;
 
     if (year == null) {
-            requestUrl = `${rootUrl}/standings/now`;
+        requestUrl = `${rootUrl}/standings/now`;
     } else {
-        if(year + 1 >= currentYear) {
+        if (year + 1 >= currentYear) {
             requestUrl = `${rootUrl}/standings/now`;
         } else {
-            requestUrl = `${rootUrl}/standings/${parseInt(year)+1}-02-01`
+            requestUrl = `${rootUrl}/standings/${parseInt(year) + 1}-02-01`
         }
     }
 
@@ -106,7 +193,7 @@ async function getStandings(year, format) {
             'Pragma': 'no-cache',
         }
     }).then(response => processResponse(response));
-    
+
     return formatStandings(standings, format);
 }
 module.exports = {
@@ -123,19 +210,29 @@ module.exports = {
                 .setName('format')
                 .setDescription('Format to retrieve, default is by division')
                 .addChoices(
-                    {name: "Conference", value: "conference"},
-                    {name: "Wildcard", value: "wildcard"},
-                    {name: "League", value: "league"},
-                    {name: "Division", value: "division"}
+                    { name: "Conference", value: "conference" },
+                    { name: "Wildcard", value: "wildcard" },
+                    { name: "League", value: "league" },
+                    { name: "Division", value: "division" }
                 )
                 .setRequired(false)),
 
     async execute(interaction) {
         const year = interaction.options.getString('year');
         const format = interaction.options.getString('format');
-        const standings = await getStandings(year, format);
 
-        await interaction.reply(`Standings for ${year == null ? 'current' : year} season: \n\n${standings}`);
+        const standingsEmbed = new EmbedBuilder()
+            .setColor(0x0099FF)
+            .setAuthor({ name: 'NHL Info Bot', iconURL: 'https://logodownload.org/wp-content/uploads/2021/06/nhl-logo.png', url: 'https://discord.js.org' })
+
+        const standings = await getStandings(year, format);
+        standingsEmbed.addFields({
+            name: 'NHL Standings', value: `${standings}`, inline: false
+        });
+        await interaction.reply({
+            embeds: [standingsEmbed],
+        })
+        //await interaction.reply(`Standings for ${year == null ? 'current' : year} season: \n\n${standings}`);
 
     }
 };
