0fad323de0623b6fd1f5c32099fffec8f3954483
erikgooding1 <erik.gooding1@gmail.com>
2024-04-03 18:12:11 -0400

    Added all current team logo functionality, and input handling for season year

--- commands/standings.js
+++ commands/standings.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder, hyperlink, EmbedBuilder, Client } = require('discord.js');
 const axios = require('axios');
 const rootUrl = 'https://api-web.nhle.com/v1';
+const teamLogos = require('./teamlogos.json');
 
 
 async function getStandings(year, format) {
@@ -29,23 +30,40 @@ async function getStandings(year, format) {
             return ' '.repeat(padding);
         }
 
+        let maxWidths;
+
+        if(parseInt(year) > 1974) {
+            maxWidths = {
+                divisionName: Math.max(...standings.map(record => record.divisionName.length)),
+                conferenceName: Math.max(...standings.map(record => record.conferenceName.length)),
+                wildcardSequence: Math.max(...standings.map(record => record.wildcardSequence.toString().length)),
+                teamName: Math.max(...standings.map(record => record.teamName.length)),
+                teamAbbrev: Math.max(...standings.map(record => record.teamAbbrev.length), 6),
+                wins: Math.max(...standings.map(record => record.wins.toString().length), 3),
+                losses: Math.max(...standings.map(record => record.losses.toString().length), 3),
+                otLosses: Math.max(...standings.map(record => record.otLosses.toString().length), 4),
+                points: Math.max(...standings.map(record => record.points.toString().length), 3),
+            };   
+        } else {
+            maxWidths = {
+                teamName: Math.max(...standings.map(record => record.teamName.length)),
+                teamAbbrev: Math.max(...standings.map(record => record.teamAbbrev.length), 6),
+                wins: Math.max(...standings.map(record => record.wins.toString().length), 3),
+                losses: Math.max(...standings.map(record => record.losses.toString().length), 3),
+                otLosses: Math.max(...standings.map(record => record.otLosses.toString().length), 4),
+                points: Math.max(...standings.map(record => record.points.toString().length), 3),
+            };
+        }
+
         // Calculate maximum widths for each property
-        const maxWidths = {
-            divisionName: Math.max(...standings.map(record => record.divisionName.length)),
-            conferenceName: Math.max(...standings.map(record => record.conferenceName.length)),
-            wildcardSequence: Math.max(...standings.map(record => record.wildcardSequence.toString().length)),
-            teamName: Math.max(...standings.map(record => record.teamName.length)),
-            teamAbbrev: Math.max(...standings.map(record => record.teamAbbrev.length), 6),
-            wins: Math.max(...standings.map(record => record.wins.toString().length), 3),
-            losses: Math.max(...standings.map(record => record.losses.toString().length), 3),
-            otLosses: Math.max(...standings.map(record => record.otLosses.toString().length), 4),
-            points: Math.max(...standings.map(record => record.points.toString().length), 3),
-        };
+        
 
         let standingsHeader = `\`TEAM${calculatePadding('TEAM', 9)}W${calculatePadding('W', maxWidths.wins)}L${calculatePadding('L', maxWidths.losses)}OTL${calculatePadding('OTL', maxWidths.otLosses)}PTS${calculatePadding('PTS', maxWidths.points)}\`\n`;
 
+        // Builds a string for each teams record provided all necessary data values, and also fetches team logo
         const buildRecordString = (record) => {
-            return `<:caps:1224753727769018368> \`${record.teamAbbrev}${calculatePadding(record.teamAbbrev, maxWidths.teamAbbrev)}${record.wins}${calculatePadding(record.wins, maxWidths.wins)}${record.losses}${calculatePadding(record.losses, maxWidths.losses)}${record.otLosses}${calculatePadding(record.otLosses, maxWidths.otLosses)}${record.points}${calculatePadding(record.points, maxWidths.points)}\`\n`;
+            let teamLogo = teamLogos[record.teamAbbrev];
+            return `${teamLogo} \`${record.teamAbbrev}${calculatePadding(record.teamAbbrev, maxWidths.teamAbbrev)}${record.wins}${calculatePadding(record.wins, maxWidths.wins)}${record.losses}${calculatePadding(record.losses, maxWidths.losses)}${record.otLosses}${calculatePadding(record.otLosses, maxWidths.otLosses)}${record.points}${calculatePadding(record.points, maxWidths.points)}\`\n`;
         }
 
         const formatConferenceStandings = (standings) => {
@@ -168,7 +186,10 @@ async function getStandings(year, format) {
             }
         }
 
-
+        if(parseInt(year) < 1975) {
+            format = 'league';
+        }
+        
         switch (format) {
             case 'conference':
                 return formatConferenceStandings(standings);
@@ -240,44 +261,59 @@ module.exports = {
         const year = interaction.options.getString('year');
         const format = interaction.options.getString('format');
 
-        const standingsEmbed = new EmbedBuilder()
+        const currDate = new Date();
+        let currentSeason = currDate.getFullYear();
+        if(currDate.getMonth() < 8) {
+            currentSeason--;
+        }
+
+        if(!(!year || (parseInt(year) >= 1917 && parseInt(year) <= currentSeason))) {
+
+            console.log("Invalid year");
+            await interaction.reply(`Invalid year. Enter a value between 1917 and ${currentSeason}`);
+
+        } else {
+            const standingsEmbed = new EmbedBuilder()
             .setColor(0x0099FF)
             .setAuthor({ name: 'NHL Info Bot', iconURL: 'https://logodownload.org/wp-content/uploads/2021/06/nhl-logo.png', url: 'https://discord.js.org' })
 
-        const standings = await getStandings(year, format);
-        switch (format) {
-            case 'conference':
-                standingsEmbed.addFields(
-                    {name: 'Eastern Conference', value: `${standings.easternStandings}`},
-                    {name: 'Western Conference', value: `${standings.westernStandings}`}
-                );
-                break;
-            case 'wildcard':
-                standingsEmbed.addFields(
-                    {name: 'Atlantic', value: `${standings.atlanticStandings}`},
-                    {name: 'Metropolitan', value: `${standings.metropolitanStandings}`},
-                    {name: 'Eastern WC', value: `${standings.eastWildcardStandings}`},
-                    {name: 'Central', value: `${standings.centralStandings}`},
-                    {name: 'Western', value: `${standings.pacificStandings}`}, 
-                    {name: 'Western WC', value: `${standings.westWildcardStandings}`}
-                );
-                break;
-            case 'league':
-                // uses setDescription due to fields having 1024 char limit
-                standingsEmbed.setDescription(`${standings.leagueStandings}`);
-                break;
-            case 'division':
-            default:
-                standingsEmbed.addFields(
-                    {name: 'Atlantic', value: `${standings.atlanticStandings}`},
-                    {name: 'Metropolitan', value: `${standings.metropolitanStandings}`},
-                    {name: 'Central', value: `${standings.centralStandings}`},
-                    {name: 'Western', value: `${standings.pacificStandings}`}
-                );
+            const standings = await getStandings(year, format);
+            switch (format) {
+                case 'conference':
+                    standingsEmbed.addFields(
+                        {name: 'Eastern Conference', value: `${standings.easternStandings}`},
+                        {name: 'Western Conference', value: `${standings.westernStandings}`}
+                    );
+                    break;
+                case 'wildcard':
+                    standingsEmbed.addFields(
+                        {name: 'Atlantic', value: `${standings.atlanticStandings}`},
+                        {name: 'Metropolitan', value: `${standings.metropolitanStandings}`},
+                        {name: 'Eastern WC', value: `${standings.eastWildcardStandings}`},
+                        {name: 'Central', value: `${standings.centralStandings}`},
+                        {name: 'Western', value: `${standings.pacificStandings}`}, 
+                        {name: 'Western WC', value: `${standings.westWildcardStandings}`}
+                    );
+                    break;
+                case 'league':
+                    // uses setDescription due to fields having 1024 char limit
+                    standingsEmbed.setDescription(`${standings.leagueStandings}`);
+                    break;
+                case 'division':
+                default:
+                    standingsEmbed.addFields(
+                        {name: 'Atlantic', value: `${standings.atlanticStandings}`},
+                        {name: 'Metropolitan', value: `${standings.metropolitanStandings}`},
+                        {name: 'Central', value: `${standings.centralStandings}`},
+                        {name: 'Western', value: `${standings.pacificStandings}`}
+                    );
+            }
+            await interaction.reply({
+                embeds: [standingsEmbed],
+            });
         }
-        await interaction.reply({
-            embeds: [standingsEmbed],
-        })
+
+        
 
     }
 };
